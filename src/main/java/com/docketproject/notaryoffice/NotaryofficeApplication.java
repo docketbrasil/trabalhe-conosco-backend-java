9b4580263ec8c6b50a349b896d084bb45e5a8a42
Denis Alves <dee.alves2@gmail.com>
2021-07-29 20:05:41 -0300

    started project

---
+++ src/main/java/com/docketproject/notaryoffice/NotaryofficeApplication.java
@@ -0,0 +1,13 @@
+package com.docketproject.notaryoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotaryofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotaryofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docketproject/notaryoffice/NotaryofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docketproject.notaryoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotaryofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
