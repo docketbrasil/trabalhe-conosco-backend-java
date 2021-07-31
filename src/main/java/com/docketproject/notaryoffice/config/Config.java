0bb7a47aa3d0d8bd8e839bf9ac991a06c4937c48
Denis Alves <dee.alves2@gmail.com>
2021-07-31 01:28:16 -0300

    Adding WebClient Config

--- src/main/java/com/docketproject/notaryoffice/config/Config.java
+++ src/main/java/com/docketproject/notaryoffice/config/Config.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.docketproject.notaryoffice.service.DBService;
 
@@ -14,18 +17,24 @@ public class Config {
 
 	@Autowired
 	private DBService dbService;
-	
+
 	@Value("${spring.jpa.hibernate.ddl-auto}")
 	private String strategy;
-	
+
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		
+
 		if (!"create".equals(strategy)) {
 			return false;
 		}
-		
+
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+
+	@Bean
+	public WebClient webClient(WebClient.Builder builder) {
+		return builder.baseUrl("https://docketdesafiobackend.herokuapp.com")
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
 	}
 }
