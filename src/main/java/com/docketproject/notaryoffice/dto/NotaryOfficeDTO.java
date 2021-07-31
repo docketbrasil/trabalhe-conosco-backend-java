e06be66db5b371a9097b375ec786c38a39a444fd
Denis Alves <dee.alves2@gmail.com>
2021-07-31 01:33:03 -0300

    Creating dtos

---
+++ src/main/java/com/docketproject/notaryoffice/dto/NotaryOfficeDTO.java
@@ -0,0 +1,59 @@
+package com.docketproject.notaryoffice.dto;
+
+public class NotaryOfficeDTO {
+
+	private String name;
+	private String email;
+	private String phone;
+	private String street;
+	private String city;
+	private String country;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
