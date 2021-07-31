689c80735ad5eaa57d0c766f0bab1b3c5290237c
Denis Alves <dee.alves2@gmail.com>
2021-07-31 01:29:29 -0300

    receiving object from template

--- src/main/java/com/docketproject/notaryoffice/controller/NotaryOfficeController.java
+++ src/main/java/com/docketproject/notaryoffice/controller/NotaryOfficeController.java
@@ -1,15 +1,29 @@
 package com.docketproject.notaryoffice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.docketproject.notaryoffice.dto.NotaryOfficeDTO;
+import com.docketproject.notaryoffice.service.NotaryOfficeService;
 
 @Controller
+@RequestMapping("notaryoffice")
 public class NotaryOfficeController {
 
-	@GetMapping("/notaryoffice")
-	public String notaryOfficeView(Model model) {
-		model.addAttribute("name", "Closed");
+	@Autowired
+	private NotaryOfficeService service;
+	
+	@GetMapping("form")
+	public String notaryOfficeView() {
+		return "notaryOffice";
+	}
+
+	@PostMapping("create")
+	public String createNotaryOffice(NotaryOfficeDTO objDto) {
+		service.insert(objDto);
 		return "notaryOffice";
 	}
 }
