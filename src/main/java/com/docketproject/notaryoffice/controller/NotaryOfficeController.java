a0e2b5d5454a32121973d28c33bba43b45e8efcc
Denis Alves <dee.alves2@gmail.com>
2021-08-02 00:13:05 -0300

    Inserting NotaryOffice

--- src/main/java/com/docketproject/notaryoffice/controller/NotaryOfficeController.java
+++ src/main/java/com/docketproject/notaryoffice/controller/NotaryOfficeController.java
@@ -2,28 +2,37 @@ package com.docketproject.notaryoffice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.docketproject.notaryoffice.dto.NotaryOfficeDTO;
+import com.docketproject.notaryoffice.service.CertificateService;
 import com.docketproject.notaryoffice.service.NotaryOfficeService;
+import com.docketproject.notaryoffice.utils.ControllerUtils;
 
 @Controller
-@RequestMapping("notaryoffice")
+@RequestMapping("notaryOffice")
 public class NotaryOfficeController {
 
 	@Autowired
-	private NotaryOfficeService service;
-	
+	private NotaryOfficeService notaryOfficeService;
+
+	@Autowired
+	private CertificateService certificateService;
+
 	@GetMapping("form")
-	public String notaryOfficeView() {
+	public String notaryOfficeView(Model model) {
+		model.addAllAttributes(ControllerUtils.fillMap(certificateService));
 		return "notaryOffice";
 	}
 
 	@PostMapping("create")
-	public String createNotaryOffice(NotaryOfficeDTO objDto) {
-		service.insert(objDto);
-		return "notaryOffice";
+	public String createNotaryOffice(@ModelAttribute NotaryOfficeDTO notaryOfficeDTO, Model model) {
+		model.addAttribute("certificates", certificateService.findAllFromApi());
+		notaryOfficeService.insert(notaryOfficeDTO);
+		return "notaryOffice/form";
 	}
 }
