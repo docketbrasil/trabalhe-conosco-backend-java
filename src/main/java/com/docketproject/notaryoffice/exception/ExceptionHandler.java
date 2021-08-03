2a205329f6f4354ce5f67dc4be0d94bc0b102ac1
Denis Alves <dee.alves2@gmail.com>
2021-08-02 22:05:07 -0300

    Adding custom exception

---
+++ src/main/java/com/docketproject/notaryoffice/exception/ExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.docketproject.notaryoffice.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+	@org.springframework.web.bind.annotation.ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Not Found",
+				e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+}
