320000b0efc06b01e40f4c9ca7a4a7a5e6285568
Denis Alves <dee.alves2@gmail.com>
2021-08-02 23:51:10 -0300

    Adding constructor

--- src/main/java/com/docketproject/notaryoffice/dto/CertificateDTO.java
+++ src/main/java/com/docketproject/notaryoffice/dto/CertificateDTO.java
@@ -5,6 +5,11 @@ public class CertificateDTO {
 	private Long id;
 	private String nome;
 
+	public CertificateDTO(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
 	public Long getId() {
 		return id;
 	}
