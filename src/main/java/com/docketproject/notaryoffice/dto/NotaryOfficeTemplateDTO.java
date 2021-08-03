b50ff8a7526a2ee5398b2c1366960dfecb828206
Denis Alves <dee.alves2@gmail.com>
2021-08-02 23:52:28 -0300

    Adding new dto to the template

---
+++ src/main/java/com/docketproject/notaryoffice/dto/NotaryOfficeTemplateDTO.java
@@ -0,0 +1,77 @@
+package com.docketproject.notaryoffice.dto;
+
+public class NotaryOfficeTemplateDTO {
+
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String street;
+	private String city;
+	private String country;
+	private String certificateName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCertificateName() {
+		return certificateName;
+	}
+
+	public void setCertificateName(String certificateName) {
+		this.certificateName = certificateName;
+	}
+}
