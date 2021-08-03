3f63a4cd2375585fa9b1fdd9efa7731c4e06f586
Denis Alves <dee.alves2@gmail.com>
2021-08-02 23:50:28 -0300

    controller still doesn't work

--- src/main/java/com/docketproject/notaryoffice/controller/NotaryOfficeController.java
+++ src/main/java/com/docketproject/notaryoffice/controller/NotaryOfficeController.java
@@ -8,17 +8,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.docketproject.notaryoffice.dto.NotaryOfficeDTO;
+import com.docketproject.notaryoffice.dto.NotaryOfficeTemplateDTO;
 import com.docketproject.notaryoffice.service.CertificateService;
-import com.docketproject.notaryoffice.service.NotaryOfficeService;
 import com.docketproject.notaryoffice.utils.ControllerUtils;
 
 @Controller
 @RequestMapping("notaryOffice")
 public class NotaryOfficeController {
-
-	@Autowired
-	private NotaryOfficeService notaryOfficeService;
 
 	@Autowired
 	private CertificateService certificateService;
@@ -30,9 +26,7 @@ public class NotaryOfficeController {
 	}
 
 	@PostMapping("create")
-	public String createNotaryOffice(@ModelAttribute NotaryOfficeDTO notaryOfficeDTO, Model model) {
-		model.addAttribute("certificates", certificateService.findAllFromApi());
-		notaryOfficeService.insert(notaryOfficeDTO);
-		return "notaryOffice/form";
+	public String createNotaryOffice(@ModelAttribute NotaryOfficeTemplateDTO notaryOfficeTemplateDTO, Model model) {
+		return "notaryOffice";
 	}
 }
