1611330326d456f631e9793bd4b866b066f71f71
Denis Alves <dee.alves2@gmail.com>
2021-08-02 23:51:56 -0300

    Change dto

--- src/main/java/com/docketproject/notaryoffice/utils/ControllerUtils.java
+++ src/main/java/com/docketproject/notaryoffice/utils/ControllerUtils.java
@@ -3,7 +3,7 @@ package com.docketproject.notaryoffice.utils;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.docketproject.notaryoffice.dto.NotaryOfficeDTO;
+import com.docketproject.notaryoffice.dto.NotaryOfficeTemplateDTO;
 import com.docketproject.notaryoffice.service.CertificateService;
 
 public class ControllerUtils {
@@ -11,7 +11,7 @@ public class ControllerUtils {
 	public static Map<String, Object> fillMap(CertificateService certificateService) {
 		Map<String, Object> mapModels = new HashMap<>();
 		mapModels.put("certificates", certificateService.findAllFromApi());
-		mapModels.put("notaryOfficeDTO", new NotaryOfficeDTO());
+		mapModels.put("notaryOfficeTemplateDTO", new NotaryOfficeTemplateDTO());
 		return mapModels;
 	}
 }
